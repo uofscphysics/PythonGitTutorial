cc904c2341f4097c7e189fddc07fc0c0f71331b3
Nicholas Tyler <tylern@canisius.edu>
2019-05-27 15:33:12 -0400

    Add example that should run travis ci

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # PythonGitTutorial
+
+
+Your task:
+
+Add json file to json_files directory.
+Make sure it prints.
---
+++ print_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import json
+import os
+
+
+path = os.getcwd() + "/json_files/"
+json_files = [
+    path + f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))
+]
+
+
+for file_name in json_files:
+    with open(file_name) as fil:
+        print(json.load(fil))
