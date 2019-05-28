3991897bbe7abf4fbc21adf806e103c935161f9b
Nicholas Tyler <tylern@canisius.edu>
2019-05-28 08:22:23 -0400

    Add new color print feature

--- print_json.py
+++ print_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import json
 import os
-
+from colorama import Fore, Style
 
 path = os.getcwd() + "/json_files/"
 json_files = [
@@ -10,4 +10,8 @@ json_files = [
 
 for file_name in json_files:
     with open(file_name) as fil:
-        print(json.load(fil))
+        json_data = json.load(fil)
+        for key in json_data["Vehicle"]:
+            print(
+                f'{Fore.GREEN}{key}{Style.RESET_ALL}: {Fore.BLUE}{json_data["Vehicle"][key]}{Style.RESET_ALL}'
+            )
