0609825fc39f0d68516b1c0d0c3b8b9ba85ea4a2
R M <ramoh87@gmail.com>
2019-06-03 15:38:54 -0400

    I changed the print
    added longer print statement

---
+++ print_json_R.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import json
+import os
+from colorama import Fore, Style
+
+path = os.getcwd() + "/json_files/"
+json_files = [
+    path + f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))
+]
+
+for file_name in json_files:
+    with open(file_name) as fil:
+        json_data = json.load(fil)
+        assert "Vehicle" in json_data
+        for key in json_data["Vehicle"]:
+            print(
+                f'{Fore.RED}{key}{Style.RESET_ALL}: {Fore.BLUE}{json_data["Vehicle"][key]}{Style.RESET_ALL}'
+            )
+        print("I am making new changes")
+
+print("Done!")
